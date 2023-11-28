2de9323ac96484011c8f7f907c8d5843fb3fd8ed
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-28 14:49:12 +1300

    Fixing a lingering merge error

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -18,15 +18,3 @@ function App() {
 }
 
 export default App
-function App() {
-  return (
-    <>
-      <header className="header">
-        <h1>My Collection</h1>
-      </header>
-      <section className="main">{/* add your code here */}</section>
-    </>
-  )
-}
-
-export default App
