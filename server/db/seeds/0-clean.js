ae9b5b786d1f1c82d2ea5304e470b9d89baa9d2d
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-22 22:08:24 +1300

    Added to clean seed file

--- server/db/seeds/0-clean.js
+++ server/db/seeds/0-clean.js
@@ -1,4 +1,14 @@
 export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('games').del()
+  await knex('platforms').del()
+  await knex('user').del()
+  await knex('story_method').del()
+  await knex('difficulty').del()
+  await knex('structure').del()
+  await knex('perspective').del()
+  await knex('genre').del()
+  await knex('artstyle').del()
+  await knex('atmosphere').del()
+  await knex('').del()
 }
