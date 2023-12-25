43923deb8dcafb7bddd0bc2169d0219691df1798
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-25 19:29:50 +1300

    Building out intermediate table entries

--- server/db/seeds/15-gamelist.js
+++ server/db/seeds/15-gamelist.js
@@ -2,9 +2,9 @@ export async function seed(knex) {
   // inserts seed entries
   await knex('gamelist').insert([
     {
-      user_id: '',
-      games_id: '',
-      platforms_id: '',
+      user_id: '6be4b31a-3796-4740-b6b7-7b419fdf1f70',
+      games_id: 'cf6da41c-7a95-41e5-b5a0-e8b540fc6c7f',
+      platforms_id: 'fb5e87fa-3ed0-4836-92b6-75cbdda819ce',
       status: 'paused',
     },
   ])
