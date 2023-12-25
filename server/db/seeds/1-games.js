6a7f45e4b97abc87044b8a5b2e425c8b7816e907
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-26 11:24:45 +1300

    Building out seed file for Armored Core VI

--- server/db/seeds/1-games.js
+++ server/db/seeds/1-games.js
@@ -3,8 +3,10 @@ export async function seed(knex) {
   await knex('games').insert([
     {
       id: 'cf6da41c-7a95-41e5-b5a0-e8b540fc6c7f',
-      publishers_id: '',
-      developers_id: '',
+      publishers_id: '42c59485-4765-430e-80c7-c1f284876973',
+      // Bandai Namco
+      developers_id: '04c305ef-a031-402b-83bb-80326eeda218',
+      // FromSoftware
       name: 'Armored Core VI: The Fires of Rubicon',
       date: '',
       atmosphere_id: '',
