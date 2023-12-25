634469a30ed51f18d6b177319956648f337d6b48
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-26 11:21:09 +1300

    Building out intermediate table entries

--- server/db/seeds/15-gamelist.js
+++ server/db/seeds/15-gamelist.js
@@ -3,8 +3,11 @@ export async function seed(knex) {
   await knex('gamelist').insert([
     {
       user_id: '6be4b31a-3796-4740-b6b7-7b419fdf1f70',
+      // johndoe
       games_id: 'cf6da41c-7a95-41e5-b5a0-e8b540fc6c7f',
+      // Armored Core VI
       platforms_id: 'fb5e87fa-3ed0-4836-92b6-75cbdda819ce',
+      // PS5
       status: 'paused',
     },
   ])
--- server/db/seeds/17-story.js
+++ server/db/seeds/17-story.js
@@ -2,9 +2,11 @@ export async function seed(knex) {
   // inserts seed entries
   await knex('story').insert([
     {
-      games_id: '',
-      story_method_id: '',
-      percentage: null,
+      games_id: 'cf6da41c-7a95-41e5-b5a0-e8b540fc6c7f',
+      // Armored Core VI
+      story_method_id: 'fb099acc-e758-4a09-8592-cc8ceba693c6',
+      // Cutscene
+      percentage: 15,
     },
   ])
 }
