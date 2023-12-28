52f45905bc459c435b11b24c75ae6e66531d016b
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-28 16:50:46 +1300

    Adding to the Armored core entries

--- server/db/seeds/8-genre.js
+++ server/db/seeds/8-genre.js
@@ -3,8 +3,9 @@ export async function seed(knex) {
   await knex('genre').insert([
     {
       id: '965c93fc-b0a4-45fa-af7a-69765ad8a0fd',
-      name: '',
-      definition: '',
+      name: 'Shooter Mecha Action',
+      definition:
+        'A game that uses firearms and other projectiles as a primary form of combat that is specific to Mechs.  Mechs being a vehicle that is piloted and carries heavy weaponry.  Mechs typically can be upgraded through acquiring and equipping parts.',
     },
   ])
 }
