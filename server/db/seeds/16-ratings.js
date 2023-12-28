94e9e97e08299cb1884d775ec27dd51fd69c003c
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-12-28 17:50:44 +1300

    Adding to the Armored core entries

--- server/db/seeds/16-ratings.js
+++ server/db/seeds/16-ratings.js
@@ -2,10 +2,21 @@ export async function seed(knex) {
   // inserts seed entries
   await knex('ratings').insert([
     {
-      games_id: '',
-      platforms_id: '',
-      source: '',
-      critic: null,
+      games_id: 'cf6da41c-7a95-41e5-b5a0-e8b540fc6c7f',
+      // Armored Core VI
+      platforms_id: 'fb5e87fa-3ed0-4836-92b6-75cbdda819ce',
+      // PS5
+      source: 'MetaCritic',
+      critic: 86,
+      user: 8,
+    },
+    {
+      games_id: 'cf6da41c-7a95-41e5-b5a0-e8b540fc6c7f',
+      // Armored Core VI
+      platforms_id: 'fb5e87fa-3ed0-4836-92b6-75cbdda819ce',
+      // PS5
+      source: 'OpenCritic',
+      critic: 85,
       user: null,
     },
   ])
