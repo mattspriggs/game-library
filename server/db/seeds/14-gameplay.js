d4c808ec5e098b95117091d0c6739776112a70bc
Matt Spriggs <matt.c.spriggs@gmail.com>
2024-01-05 13:44:17 +1300

    Adding gameplay_types and adding Armored Core VI info for DB seed file

--- server/db/seeds/14-gameplay.js
+++ server/db/seeds/14-gameplay.js
@@ -15,5 +15,19 @@ export async function seed(knex) {
       // Armored Core VI
       percentage: 30,
     },
+    {
+      gameplay_type_id: 'b6cdc8cf-2377-4623-bb96-dee6d9552a0c',
+      // Complex traversal
+      games_id: 'cf6da41c-7a95-41e5-b5a0-e8b540fc6c7f',
+      // Armored Core VI
+      percentage: 20,
+    },
+    {
+      gameplay_type_id: '9332352f-1489-4b7c-ac98-92bcc31b8e71',
+      // Exploration
+      games_id: 'cf6da41c-7a95-41e5-b5a0-e8b540fc6c7f',
+      // Armored Core VI
+      percentage: 10,
+    },
   ])
 }
